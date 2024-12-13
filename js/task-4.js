45ffbcddb01a36a1230cef21378e07c46b3421c8
MykhailoPetrovkyi <petrovskij65@gmail.com>
2024-12-13 11:10:23 +0200

    Add a return to the if

--- js/task-4.js
+++ js/task-4.js
@@ -7,6 +7,7 @@ form.addEventListener('submit', (e) => {
   const password = form.elements.password.value.trim()
   if (email === '' || password === '') {
     alert('All form fields must be filled in')
+    return
   }
 
   const userData = {
